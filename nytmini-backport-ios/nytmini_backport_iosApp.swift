acb54afa71b4e9a1ddc314a2e7a5e7ba6dde65a5
james-magee <jam020201@gmail.com>
2024-02-03 17:59:21 -0800

    Initial Commit

---
+++ nytmini-backport-ios/nytmini_backport_iosApp.swift
@@ -0,0 +1,32 @@
+//
+//  nytmini_backport_iosApp.swift
+//  nytmini-backport-ios
+//
+//  Created by James Magee on 2/3/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct nytmini_backport_iosApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
